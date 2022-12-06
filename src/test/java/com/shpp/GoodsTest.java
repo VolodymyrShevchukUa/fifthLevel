8a96dcc99fb8fb8245bc657366742e2d23ddef9c
Vova Shevchuk <volodymyr.shevchuk.ua@gmail.com>
2022-12-06 12:38:13 +0200

    Changes assertEquals

--- src/test/java/com/shpp/GoodsTest.java
+++ src/test/java/com/shpp/GoodsTest.java
@@ -27,13 +27,13 @@ class GoodsTest {
         Goods goods = new Goods("Name", null,51.2);
         Set<ConstraintViolation<Goods>> goodsValidator = validator.validate(goods);
         Assertions.assertEquals(1, goodsValidator.size());
-        assertEquals("не должно равняться null",goodsValidator.iterator().next().getMessage());
+        assertEquals("must not be null",goodsValidator.iterator().next().getMessage());
     }
     @Test
     void ValidationPrice(){
         Goods goods = new Goods("Name", new Category("ss"),49.9);
         Set<ConstraintViolation<Goods>> goodsValidator = validator.validate(goods);
         Assertions.assertEquals(1, goodsValidator.size());
-        assertEquals("должно быть не меньше 50",goodsValidator.iterator().next().getMessage());
+        assertEquals("must be greater than or equal to 50",goodsValidator.iterator().next().getMessage());
     }
 }
